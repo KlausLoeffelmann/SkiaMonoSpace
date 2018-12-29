a98806ea7e5f098c8d74e106f5b990b6e06e29e8
Klaus Löffelmann <klloeffe@microsoft.com>
2018-12-29 17:28:30 +0100

    Initial checkin.

---
+++ src/SkiaMonoSpaceDemo/SkiaMonoSpace.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp.Views.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiaMonoSpaceDemo
+{
+    class SkiaMonoSpace : SKGLControl
+    {
+        protected override void OnPaintSurface(SKPaintGLSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+            
+        }
+    }
+}
---
+++ src/SkiaMonoSpaceRenderer/SkiaMonoSpaceRenderer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SkiaMonoSpaceRenderer
+{
+    public class SkiaMonoSpaceRenderer
+    {
+    }
+}
